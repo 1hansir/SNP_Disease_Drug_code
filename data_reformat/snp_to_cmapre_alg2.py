7cc1f94ba4b720b1458748bc15b987fdfc9dcc00
North <1615474527@qq.com>
2023-07-23 12:33:30 -0400

    update the indexing algorithm with dictionary(faster) of snp_to_cmapre.py

--- data_reformat/snp_to_cmapre_alg2.py
+++ data_reformat/snp_to_cmapre_alg2.py
@@ -9,8 +9,10 @@ cmap_ko_filename = 'CMap/Format_data/Maxpool/ko.csv'
 gene_cmap_info_filename = "/CMap/Datasets/geneinfo_beta.txt"
 snp_pairs = pd.read_csv(mdir + snp_pair_filename)
 oe_vec = pd.read_csv(mdir + cmap_oe_filename,index_col=['gene'])
-# oe_vec_init = pd.read_csv(mdir + cmap_oe_filename)
+oe_dict = oe_vec.T.to_dict(orient='series')
 ko_vec = pd.read_csv(mdir + cmap_ko_filename,index_col=['gene'])
+ko_dict = ko_vec.T.to_dict(orient='series')
+
 cmap_gene_file = pd.read_csv(mdir + gene_cmap_info_filename, sep="\t", dtype=str)
 gene_info = cmap_gene_file['ensembl_id'].unique()
 
@@ -43,7 +45,7 @@ flag = 0
 count_miss = 0
 snp_name = []
 
-for i in range(0, round(len(snp_list)/1000)):
+for i in range(0, len(snp_list)):
     snp_index_i = snp_list[i]
     pairs = snp_pairs[snp_pairs['snp_index'] == snp_index_i]
     snp_vec_i = vec_0
@@ -56,19 +58,26 @@ for i in range(0, round(len(snp_list)/1000)):
         slope_i_j = slope_list_i[j]
         gene_i_j = gene_list_i[j]
 
-        if slope_i_j > 0 and (gene_i_j in gene_oe):
-            vec_add = slope_i_j * oe_vec.loc[gene_i_j]
-            flag_i = 1
-        elif slope_i_j < 0 and (gene_i_j in gene_ko):
-            vec_add = slope_i_j * ko_vec.loc[gene_i_j]
-            flag_i = 1
-        else:
-            count_miss += 1
+        if slope_i_j > 0:
+            try:
+                vec_add = slope_i_j * oe_dict[gene_i_j]
+                flag_i = 1
+            except KeyError:
+                count_miss += 1
+                continue
+
+        elif slope_i_j < 0:
+            try:
+                vec_add = slope_i_j * ko_dict[gene_i_j]
+                flag_i = 1
+            except KeyError:
+                count_miss += 1
+                continue
 
         snp_vec_i = snp_vec_i + vec_add
 
     if i % 10000 == 0:
-            print(i/len(snp_list))
+        print(i/len(snp_list))
 
     if flag_i == 0:
         continue
